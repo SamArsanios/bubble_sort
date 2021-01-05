b0a6eac0e89d5c3f7eca62f680beb1ab0b694749
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-01-05 13:10:03 +0100

    fix linter error

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -1,15 +1,15 @@
 def bubble_sort(arr)
-	len = arr.length
-	swap = true
-	  while swap
-		swap = false
-		(0...len-1).each do |i|
-		  if arr[i] > arr[i+1]
-			arr[i],arr[i+1] = arr[i+1], arr[i]
-			swap = true
-		  end
-		end
-	  end
-	  return arr
+  len = arr.length
+  swap = true
+  while swap
+    swap = false
+    (0...len - 1).each do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swap = true
+      end
+    end
+  end
+  arr
 end
-	p bubble_sort([12,56,28,5,1100,6])
+p bubble_sort([12, 56, 28, 5, 1100, 6])
